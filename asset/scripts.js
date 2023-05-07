540171d641889068e716d0583a1c5e2d1faa232a
Hasan Mahmud <hmahmud01@gmail.com>
2023-05-07 23:37:48 +0600

    first draft

---
+++ asset/scripts.js
@@ -0,0 +1,19 @@
+const menuBtn = document.getElementById('menu-btn');
+const navbar = document.getElementById('navbar');
+const menu = document.querySelector('.menu');
+
+const offset = 50;
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-open');
+});
+
+
+
+window.addEventListener("scroll", () => {
+    if (pageYOffset > offset) {
+        navbar.classList.add('navbar-active');
+    }else{
+        navbar.classList.remove('navbar-active');
+    }
+});
